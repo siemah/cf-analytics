687223331a3f20c749e394485c93ee87eea8548e
siemah <hmi.dayenio@gmail.com>
2023-07-04 08:30:07 +0100

    db:create necessary tables

--- src/schemas/schema.sql
+++ src/schemas/schema.sql
@@ -1,30 +1,27 @@
-CREATE TABLE IF NOT EXISTS analytics (
+-- DROP TABLE IF EXISTS statytics;
+CREATE TABLE IF NOT EXISTS statytics (
   id integer PRIMARY KEY AUTOINCREMENT,
-  uuid text NOT NULL,
-  url text NOT NULL,
-  ip text NOT NULL,
-  longitude text NOT NULL,
-  latitude text NOT NULL,
-  country text NOT NULL,
-  city text NOT NULL,
-  region text NOT NULL,
-  regionCode text NOT NULL,
-  asOrganization text NOT NULL,
-  postalCode text NOT NULL,
-  dataCenterCode text NOT NULL,
-  browser text NOT NULL,
-  os, text NOT NULL,
-  clientAcceptEncoding text NOT NULL,
-  tlsVersion text NOT NULL,
-  timezone text NOT NULL,
-  httpProtocol text NOT NULL,
-  language text NOT NULL,
-  createdAt timestamp NOT NULL
+  uuid TEXT NULL,
+  url TEXT NULL,
+  ip TEXT NULL,
+  longitude TEXT NULL,
+  latitude TEXT NULL,
+  country TEXT NULL,
+  city TEXT NULL,
+  region TEXT NULL,
+  regionCode TEXT NULL,
+  asOrganization TEXT NULL,
+  postalCode TEXT NULL,
+  dataCenterCode TEXT NULL,
+  browser TEXT NULL,
+  os, TEXT NULL,
+  clientAcceptEncoding TEXT NULL,
+  tlsVersion TEXT NULL,
+  timezone TEXT NULL,
+  httpProtocol TEXT NULL
 );
-CREATE INDEX idx_analytics_id ON analytics (id);
-CREATE INDEX idx_analytics_country ON analytics (country);
-CREATE INDEX idx_analytics_region ON analytics (region);
-CREATE INDEX idx_analytics_os ON analytics (os);
-CREATE INDEX idx_analytics_browser ON analytics (browser);
-
--- Optionally, uncomment the below query to create data
+CREATE INDEX idx_statytics_id ON statytics (id);
+CREATE INDEX idx_statytics_url ON statytics (url);
+CREATE INDEX idx_statytics_country ON statytics (country);
+CREATE INDEX idx_statytics_region ON statytics (region);
+CREATE INDEX idx_statytics_browser ON statytics (browser);
