c27c7c214a1aea3b9ad853957adb6931d5f6e3d1
siemah <hmi.dayenio@gmail.com>
2023-08-01 13:50:35 +0100

    db:use Turso as a db replacer for D1

--- core/drizzle.config.ts
+++ core/drizzle.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "drizzle-kit";
+import { config } from 'dotenv'
+config();
 
 export default {
+  dbCredentials: {
+    url: `${process.env.turso_database_url}`,
+    authToken: `${process.env.turso_database_token}`,
+  },
   schema: "./src/db/schema/*",
-  driver: "better-sqlite",
-  out: "./src/db/migration/"
+  driver: "turso",
+  out: "./src/db/turso-migration/"
 } satisfies Config;
